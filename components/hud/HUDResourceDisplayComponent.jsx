eae0d003aeaffb69e8425a0c3b424e9d9a1733d3
Felipe Baytelman <felipe.baytelman@gmail.com>
2017-07-26 09:50:51 -0400

    Granary adds capacity Fix spending (added test)

--- components/hud/HUDResourceDisplayComponent.jsx
+++ components/hud/HUDResourceDisplayComponent.jsx
@@ -2,23 +2,21 @@ const React = require('react');
 const ResourceIcon = require('../resources/ResourceIconComponent.jsx').default;
 
 var ResourceDisplay = React.createClass({
-  buildChildren: function (resource) {
-    if (resource.amount) {
-      return (<ResourceIcon
-        key={resource.id}
-        name={resource.type}
-        amount={resource.amount}
-        max={this.props.player.initialCapacity[resource.type]}/>);
-    }
-    return null;
-  },
   render: function () {
+    let capacity = this
+      .props
+      .player
+      .getCapacity();
     let resources = Object.values(this.props.player.resources);
     return (
       <div id='resource-display' className='hud-window'>
         <span>ResourceDisplay</span>
         <resources className='resources'>
-          {resources.map(this.buildChildren)}
+          {resources.map((resource) => (resource.amount && <ResourceIcon
+            key={resource.id}
+            name={resource.type}
+            amount={resource.amount}
+            max={capacity[resource.type]}/>))}
         </resources>
       </div>
     );
