63b68176ac91e639c2f5227be03bf36cd5a6a57a
Felipe Baytelman <felipe.baytelman@gmail.com>
2017-08-01 00:21:10 -0400

    Multiply resources

--- test/resources_capacity_tests.js
+++ test/resources_capacity_tests.js
@@ -18,47 +18,70 @@ let playerCapacity = {
   }
 };
 
-
-    let building = new Building({
-      time: 100,
-      permanentEffects: [new CapacityGrantingEffect({
-          [resourceType2]: amount * 3,
-          [resourceType3]: amount * 1
-        })]
-    });
+let building = new Building({
+  time: 100,
+  permanentEffects: [new CapacityGrantingEffect({
+    additions: {
+      [resourceType2]: amount * 3,
+      [resourceType3]: amount * 1
+    }
+  })]
+});
 
 describe('Resources', () => {
-
+  
   let multiplier = 10;
-
+  
   it('Player has capacity', () => {
     let player = new CityPlayer(playerCapacity);
-
+    
     const capacity = player.getCapacity();
     assert.strictEqual(capacity[resourceType1] || 0, amount * 2);
     assert.strictEqual(capacity[resourceType2] || 0, amount * 5);
     assert.strictEqual(capacity[resourceType3] || 0, 0);
   });
-
+  
   it('Player capacity can be increased by buildings', () => {
     let player = new CityPlayer(playerCapacity);
-
+    
     player
-      .city
-      .planBuilding({building: building});
-
+    .city
+    .planBuilding({building: building});
+    
     const capacity = player.getCapacity();
     assert.strictEqual(capacity[resourceType1] || 0, amount * 2);
     assert.strictEqual(capacity[resourceType2] || 0, amount * 5);
     assert.strictEqual(capacity[resourceType3] || 0, 0);
-
+    
     player.updateTime(building.buildingTime);
     const capacityAfter = player.getCapacity();
     assert.strictEqual(capacityAfter[resourceType1] || 0, amount * 2);
     assert.strictEqual(capacityAfter[resourceType2] || 0, amount * 8);
     assert.strictEqual(capacityAfter[resourceType3] || 0, amount * 1);
   });
-
+  
+  it('Player capacity can be multiplied by buildings', () => {
+    let player = new CityPlayer(playerCapacity);
+    
+    player
+    .city
+    .planBuilding({
+      building: new Building({
+        time: 0,
+        permanentEffects: [new CapacityGrantingEffect({
+          multipliers: {
+            [resourceType1]: 0.5 // +50%
+          }
+        })]
+      })
+    });
+    
+    const capacityAfterMultiply = player.getCapacity();
+    assert.strictEqual(capacityAfterMultiply[resourceType1] || 0, amount * 3);
+    assert.strictEqual(capacityAfterMultiply[resourceType2] || 0, amount * 5);
+    
+  });
+  
   it('can only earn up to a limit', () => {
     let player = new CityPlayer({
       initialCapacity: {
@@ -66,29 +89,29 @@ describe('Resources', () => {
         [resourceType2]: amount * 5
       }
     });
-
+    
     player
-      .city
-      .planBuilding({building: building});
-
+    .city
+    .planBuilding({building: building});
+    
     player.earnResources([
       createResource1(amount * 3),
       createResource2(amount * 3),
       createResource3(amount * 3)
     ]);
-
+    
     assert.strictEqual(amount * 2, player.getResourceAmountForType(resourceType1));
     assert.strictEqual(amount * 3, player.getResourceAmountForType(resourceType2));
     assert.strictEqual(0, player.getResourceAmountForType(resourceType3));
-
+    
     player.updateTime(building.buildingTime);
-
+    
     player.earnResources([
       createResource1(amount * 3),
       createResource2(amount * 3),
       createResource3(amount * 3)
     ]);
-
+    
     assert.strictEqual(amount * 2, player.getResourceAmountForType(resourceType1));
     assert.strictEqual(amount * 6, player.getResourceAmountForType(resourceType2));
     assert.strictEqual(amount * 1, player.getResourceAmountForType(resourceType3));
