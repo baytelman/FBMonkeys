e13e2c5734caec89963bdb04f4668bd82089d9e9
Felipe Baytelman <felipe.baytelman@gmail.com>
2017-08-04 22:16:34 -0400

    Effects get paused (blocked) if player cannot earn more resources

--- test/building_resources_tests.js
+++ test/building_resources_tests.js
@@ -15,29 +15,25 @@ const PlayerEarnResourceEffect = CityPlayerJS.PlayerEarnResourceEffect;
 
 const kGold = 'gold';
 const gold = (amount) => new CityResource(kGold, amount);
+const amount = 100;
 
 describe('Buildings Effects', () => {
-  let resource = gold(100);
+  let resource = gold(amount);
+    let resources = [resource];
+  let time = 10;
+  let building = new Building({
+    effects: [new PlayerEarnResourceEffect({resources: resources, frequency: time})]
+  });
 
   it('can grant resources', () => {
-    let resources = [resource];
-    let time = 10;
-    let building = new Building({
-      effects:[
-        new PlayerEarnResourceEffect({
-          resources:resources,
-          frequency:time
-        })
-      ],
-    });
     let player = new CityPlayer({
-			initialCapacity: {
-				[kGold]: 1000
-			}
-		});
-    player.city.planBuilding({
-      building: building
+      initialCapacity: {
+        [kGold]: 1000
+      }
     });
+    player
+      .city
+      .planBuilding({building: building});
     let multiplier = 4;
     let moreResources = CityResource.resourcesWithMultiplier(resources, multiplier);
 
@@ -48,8 +44,34 @@ describe('Buildings Effects', () => {
     updates = player.updateTime(time);
     assert.isTrue(player.canAfford(moreResources));
 
-		const b = Object.values(player.city.buildings)[0];
-		assert.include(b.toString(), 'Producing');
-		assert.include(b.effects[0].toString(), 'Producing');
+    const b = Object.values(player.city.buildings)[0];
+    assert.include(b.toString(), 'Producing');
+    assert.include(b.effects[0].toString(), 'Producing');
+  });
+
+  it('are paused when limit is met', () => {
+    let mult = 10;
+    let building = new Building({
+      effects: [new PlayerEarnResourceEffect({resources: resources, frequency: time})]
+    });
+    let player = new CityPlayer({
+      initialCapacity: {
+        [kGold]: amount * mult
+      }
+    });
+    player
+      .city
+      .planBuilding({building: building});
+
+    player.updateTime(1);
+    let effect = Object.values(player.city.buildings)[0].effects[0];
+    assert.isTrue(effect instanceof PlayerEarnResourceEffect);
+
+    /* Let's speed up the time AFTER we have all the resources we can acumulate */
+    player.updateTime(time * mult/2);
+    assert.equal(effect.blocked, false);
+
+    player.updateTime(time * mult/2 + 1);
+    assert.equal(effect.blocked, true);
   });
 });
