d123b08a1aecda1cfb5737ea4b849564d94b9df9
Felipe Baytelman <felipe.baytelman@gmail.com>
2017-08-10 17:50:46 -0700

    wip

--- test/character_tests.js
+++ test/character_tests.js
@@ -7,6 +7,7 @@ import {CityResource} from '../lib/city/CityResource.js';
 import {Building} from '../lib/city/Building.js';
 import {CityPlayer, PlayerEarnResourceEffect} from '../lib/city/CityPlayer.js';
 import {CityCharacter} from '../lib/city/CityCharacter.js';
+import {CityPlayer, PlayerEarnResourceEffect, BuildingStoreResourceEffect} from '../lib/city/CityPlayer.js';
 
 const kCharacter = 'character';
 const character = (amount) => new CityResource(kCharacter, amount);
@@ -21,14 +22,38 @@ describe('Player\'s Characters', () => {
       [kCharacter]: () => new CityCharacter({name: charName})
     }
   });
-
+  
   it('Are listed', () => {
     const player = newPlayer();
     assert.equal(0, Object.values(player.city.characters).length);
-
+    
     player.earnResources([character(2)]);
     player.updateTime(1);
-
+    
     assert.equal(2, Object.values(player.city.characters).length);
+  });
+  
+  const kGold = 'gold';
+  const gold = (amount) => new CityResource(kGold, amount);
+  const amount = 100;
+  
+  it('Can be assigned roles', () => {
+    const player = newPlayer();
+    player.earnResources([character(2)]);
+    player.updateTime(1);
+    
+    const char1 = Object.values(player.city.characters)[0];
+    const char2 = Object.values(player.city.characters)[1];
+    
+    let resource = gold(amount);
+    let resources = [resource];
+    let time = 10;
+    
+    const tasks = new CityTask({
+      effects: [new PlayerEarnResourceEffect({resources: resources, frequency: time})]
+    });
+    
+    char2.assignTasks([role1]);
+    
   });
 });
