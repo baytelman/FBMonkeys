9a4dd0769167d739264f3c3c63308eb219961e9e
Felipe Baytelman <felipe.baytelman@gmail.com>
2017-08-11 10:33:23 -0700

    More wip

--- test/character_tests.js
+++ test/character_tests.js
@@ -7,16 +7,24 @@ import {CityResource} from '../lib/city/CityResource.js';
 import {Building} from '../lib/city/Building.js';
 import {CityPlayer, PlayerEarnResourceEffect} from '../lib/city/CityPlayer.js';
 import {CityCharacter} from '../lib/city/CityCharacter.js';
-import {CityPlayer, PlayerEarnResourceEffect, BuildingStoreResourceEffect} from '../lib/city/CityPlayer.js';
 
 const kCharacter = 'character';
 const character = (amount) => new CityResource(kCharacter, amount);
 
 describe('Player\'s Characters', () => {
-  let charName = 'character name';
-  let newPlayer = () => new CityPlayer({
+  
+  const kGold = 'gold';
+  const gold = (amount) => new CityResource(kGold, amount);
+  const kWood = 'wood';
+  const wood = (amount) => new CityResource(kWood, amount);
+  const amount = 100;
+  
+  const charName = 'character name';
+  const newPlayer = () => new CityPlayer({
     initialCapacity: {
-      [kCharacter]: 3
+      [kCharacter]: 3,
+      [kGold]: amount * 2,
+      [kWood]: amount * 10,
     },
     characterFactories: {
       [kCharacter]: () => new CityCharacter({name: charName})
@@ -31,29 +39,41 @@ describe('Player\'s Characters', () => {
     player.updateTime(1);
     
     assert.equal(2, Object.values(player.city.characters).length);
+    const char1 = Object.values(player.city.characters)[0];
+    const char2 = Object.values(player.city.characters)[1];
+    assert.equal(charName, char1.name);
+    assert.equal(charName, char2.name);
   });
   
-  const kGold = 'gold';
-  const gold = (amount) => new CityResource(kGold, amount);
-  const amount = 100;
-  
-  it('Can be assigned roles', () => {
+  it('Can be assigned tasks, rotate as completed', () => {
     const player = newPlayer();
     player.earnResources([character(2)]);
     player.updateTime(1);
     
     const char1 = Object.values(player.city.characters)[0];
-    const char2 = Object.values(player.city.characters)[1];
     
-    let resource = gold(amount);
-    let resources = [resource];
-    let time = 10;
+    let time = 10;    
+    const task1 = new PlayerEarnResourceEffect({resources: [gold(amount)], frequency: time});
+    const task2 = new PlayerEarnResourceEffect({resources: wood(amount), frequency: time});
     
-    const tasks = new CityTask({
-      effects: [new PlayerEarnResourceEffect({resources: resources, frequency: time})]
-    });
+    char1.tasks = [task1, task2];
+    player.updateTime(1);
     
-    char2.assignTasks([role1]);
+    assert.equal(char1.activeTask, task1);
+    player.updateTime(time);
     
+    assert.isTrue(player.canAfford([gold(amount)]));
+    assert.isFalse(player.canAfford([gold(2 * amount)]));
+    
+    assert.equal(char1.activeTask, task1);
+    player.updateTime(time);
+    
+    assert.isTrue(player.canAfford([gold(2 * amount)]));
+    assert.isFalse(player.canAfford([wood(amount)]));
+    
+    assert.equal(char1.activeTask, task2);
+    player.updateTime(time);
+    assert.isTrue(player.canAfford([wood(amount)]));
+    assert.isFalse(player.canAfford([gold(3 * amount)]));
   });
 });
